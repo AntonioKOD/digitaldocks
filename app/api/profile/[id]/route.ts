bfff44bd27293e6525a7e44ee9136a3e9c0c3dd5
Your Name <you@example.com>
2025-01-22 10:58:51 -0500

    new changes

--- app/api/profile/[id]/route.ts
+++ app/api/profile/[id]/route.ts
@@ -1,23 +1,19 @@
 import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
+import {PrismaClient} from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export async function GET(req: Request, { params }: { params: { id: string } } ) {
-    const { id } = params; // Correct way to access params
-
-    try {
-        const user = await prisma.user.findUnique({
-            where: { id }
-        });
-
-        if (!user) {
-            return NextResponse.json({ error: "User not found" }, { status: 404 });
+export async function GET(req: Request, {params}: {params: {id: string}}){
+   const {id} = params;
+    const user = await prisma.user.findUnique({
+        where: {
+            id: id
         }
-
-        return NextResponse.json({ user });
-    } catch (error) {
-        console.error(error);
-        return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+    });
+    if(!user){
+        return NextResponse.json({ error: 'User not found' }, { status: 404 });
     }
+    return NextResponse.json({user});
+ 
 }
+
